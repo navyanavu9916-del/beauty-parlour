74c0b20b2c7d641c2dfa3c92a3bd218b9ad09159
navya <navyanavu9916-del>
2026-04-08 12:02:44 +0530

    my django beauty parlour project

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+]
---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def home(request):
+     return render(request, 'home/index.html')
+def about(request):
+        return render(request, 'home/about.html')
+def contact(request):
+        return render(request, 'home/contact.html')
+def home_page(request):
+        return render(request, 'home/home.html')
+# Create your views here.
